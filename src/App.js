15f7962ddc9ff48e4a8c1747ff0a16238b82dae3
rvlopes <rv.lopes@campus.fct.unl.pt>
2022-03-25 20:21:00 +0000

    tailwind setup ready :b:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 class="text-3xl font-bold">
+        Hello world!
+      </h1>
     </div>
   );
 }
