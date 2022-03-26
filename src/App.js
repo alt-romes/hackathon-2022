659ed0d7213915072a3158956e033fe778104e16
rvlopes <rv.lopes@campus.fct.unl.pt>
2022-03-26 02:33:50 +0000

    strognoff de natas com arrozinho

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import {
 } from "react-router-dom";
 import TablePage from "./pages/TablePage";
 import PageWrapper from "./pages/PageWrapper";
-import { Component } from "react";
-import DynamicTable from "./components/DynamicTable";
 import RedirectButton from "./components/RedirectButton";
+import CustomForm from "./components/CustomForm";
 
 function App() {
+  let navigate = useNavigate();
   return (
     <div className="App">
       <PageWrapper props={
           <Routes>
             <Route path="/A" element={<> <TablePage /> <RedirectButton page="B"/> </>} />
-            <Route path="/B" element={"PAGINA B CUARAGO"}/>
+            <Route path="/B" element={<> <CustomForm fieldNames={["Name","Age","Nationality"]}/> </>}/>
           </Routes>
       } />
 
---
+++ src/components/CustomForm.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+
+export default function CustomForm({ fieldNames }) {
+    const [inputObj, setInputObj] = useState({})
+
+    const handleChange = (event, index) => {
+        let obj = {...inputObj}
+        obj[fieldNames[index]] = event.target.value
+        setInputObj(obj)
+    }
+
+    const handleSubmit = (event) => {
+
+    }
+
+    useEffect(() => {
+        console.log(inputObj)
+    }, [inputObj])
+
+    const renderInputs = fieldNames.map((fieldN, i) => (
+        <div>
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                {fieldN}
+            </label>
+            <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id={i + fieldN}
+                type="text"
+                placeholder={fieldN}
+                onChange={event => handleChange(event, i)} />
+        </div>
+    ))
+
+    return (
+        <form className="w-full max-w-lg">
+            {renderInputs}
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </form>
+    )
+}
