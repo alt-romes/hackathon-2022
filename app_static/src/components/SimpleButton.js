0f39f311bcc14b58229d534126b75ebd49017a7e
romes <rodrigo.m.mesquita@gmail.com>
2022-03-26 13:19:41 +0000

    Prettier button

--- app_static/src/components/SimpleButton.js
+++ app_static/src/components/SimpleButton.js
@@ -6,14 +6,10 @@ function SimpleButton({handleClick, text}) {
     let navigate = useNavigate();
 
     return (
-        <div>
-            <button
-            onClick={handleClick} 
-            className="bg-teal hover:bg-white hover:text-black text-white font-bold py-2 px-4 border border-black rounded"
-            >
-                {text}
-            </button>
-        </div>
+        <button type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+                onClick={handleClick} >
+            {text}
+        </button>
     )
 }
 
