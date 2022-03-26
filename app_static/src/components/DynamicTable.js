0a34284fd9b5fc08b58f43c15b1846ed6f47f024
rvlopes <rv.lopes@campus.fct.unl.pt>
2022-03-26 11:06:47 +0000

    omg throw

--- app_static/src/components/DynamicTable.js
+++ app_static/src/components/DynamicTable.js
@@ -2,15 +2,15 @@ import TableRow from "./TableRow"
 
 function DynamicTable({fieldNames}) {
 
-    const renderRows = table.map((row,i) => (
-        <TableRow key={i+row[0]} rowValues={row}/>
-    ))
-   
     const table = [
         ['Ricardo', '22','PT'],
         ['Alberto', '22','PT'],
         ['Rodrigo', '22','PT']
     ]
+    
+    const renderRows = table.map((row,i) => (
+        <TableRow key={i+row[0]} rowValues={row}/>
+    ))
 
     return (
         <table className="w-full mx-auto text-sm text-left text-white">
