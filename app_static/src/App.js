a7ae95ec9ade42f106375cc1f3d547359d35deaa
romes <rodrigo.m.mesquita@gmail.com>
2022-03-26 15:42:37 +0000

    attempts to fix jsx x js bug lol

--- app_static/src/App.js
+++ app_static/src/App.js
@@ -1,24 +1,34 @@
+
 import {
   Routes,
   Route,
   useNavigate
 } from "react-router-dom";
+import DynamicTable from "./components/DynamicTable";
 import PageWrapper from "./pages/PageWrapper";
 import SimpleButton from "./components/SimpleButton";
 import CustomForm from "./components/CustomForm";
-import DynamicTable from "./components/DynamicTable";
 import SpaceSplit from "./components/SpaceSplit";
-
 function App() {
   let navigate = useNavigate();
   return (
     <div className="App">
       <PageWrapper props={
           <Routes>
-            <Route path="/A" element={<> 
-            <DynamicTable tableFields={["Name","Age","Nationality"]} /> 
-            <SimpleButton handleClick={() => navigate('B')} text="Submit"/> </>} />
-            <Route path="/B" element={<> <CustomForm formFields={[['Name',' N'],['Age','N'],['Nationality','L']]}/> </>}/>
+<Route path='/A' element={<><DynamicTable
+              tableFields={["Data","Protocolo","ComoCorreu"]} /><SpaceSplit
+                  /><SimpleButton handleClick={() => navigate('/B')} text='Add'
+                      /></>} />
+
+<Route path='/B' element={<><SpaceSplit /><CustomForm
+              formFields={[["Data",'N'], ["Protocolo",'N'], ["ComoCorreu",'N'],
+              ["Foto",'U'], ["Conclusao",'L']]} submitRoute='A'
+              submitText='Submit' /></>} />
+
+<Route path='/C' element={<><DynamicTable
+              tableFields={["Data","Protocolo","ComoCorreu","Foto","Conclusao"]}
+              /></>} />
+
           </Routes>
       } />
 
@@ -27,3 +37,4 @@ function App() {
 }
 
 export default App;
+    
--- app_static/src/components/CustomForm.jsx
+++ app_static/src/components/CustomForm.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function CustomForm({ formFields }) {
     const [inputObj, setInputObj] = useState({})
@@ -14,7 +14,6 @@ export default function CustomForm({ formFields }) {
         console.log(inputObj)
     }
 
-
     const renderInputs = formFields.map((fieldN, i) => (
         fieldN[1] === "N" ? (
             <div key={fieldN[0] + i} className="mb-6">
@@ -23,7 +22,6 @@ export default function CustomForm({ formFields }) {
                 </label>
                 <input
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    id={i + fieldN[0]}
                     type="text"
                     placeholder={fieldN[0]}
                     onChange={event => handleChange(event, i)} />
@@ -36,14 +34,13 @@ export default function CustomForm({ formFields }) {
                     </label>
                     <textarea
                         className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        id={i + fieldN[0]}
                         type="text"
                         placeholder={fieldN[0]}
                         rows="3"
                         onChange={event => handleChange(event, i)} />
                 </div>
             ) : (
-                <div class="mb-6">
+                <div key={i} className="mb-6">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Upload file</label>
                     <input type="file" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 </div>
@@ -52,9 +49,12 @@ export default function CustomForm({ formFields }) {
     ))
 
     return (
-        <form>
-            {renderInputs}
-            <button onClick={handleSubmit} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-        </form>
+
+        <div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <form>
+                {renderInputs}
+                <button onClick={handleSubmit} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </form>
+        </div>
     )
 }
