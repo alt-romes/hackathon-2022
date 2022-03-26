56864e6f9819618fc05329e246387587f9f84fcf
romes <rodrigo.m.mesquita@gmail.com>
2022-03-26 19:25:44 +0000

    dynamic body

--- app_static/src/components/DynamicTable.jsx
+++ app_static/src/components/DynamicTable.jsx
@@ -44,7 +44,7 @@ function DynamicTable({ tableFields }) {
 
     return (
 
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-6">
             {
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
