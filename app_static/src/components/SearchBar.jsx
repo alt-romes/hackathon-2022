02541a8118337f040482f64cd0ed162405edf018
romes <rodrigo.m.mesquita@gmail.com>
2022-03-27 11:47:16 +0100

    tweak search bar

--- app_static/src/components/SearchBar.jsx
+++ app_static/src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ export default function SearchBar() {
 
 
     return (
-        <div className="pt-2 relative mx-auto text-gray-600 mb-2">
+        <div className="float-right mr-2 pt-2 relative mx-auto text-gray-600 mb-2">
             <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
                 type="search" name="search" placeholder="Search" />
             <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
